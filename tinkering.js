8d98c8dcf4cbf6f66987032f4746acb68a4c6d82
Yamac Ozdil <yamac.ozdil@outlook.com>
2021-01-14 22:36:01 +0000

    For..Of Loop

--- tinkering.js
+++ tinkering.js
@@ -1,5 +1,8 @@
-for (var row = 0; row <= 25; row++) {
-  for (var column = 0; column <= 99; column++) {
-  console.log(row + "-" + column); 
-  }
+const amounts = [61.00, 52.25, 112.99, 5.00]; 
+
+let total = 0;
+
+for (let amount of amounts) {
+  total += amount;
 }
+console.log('Order total is: ', total);
