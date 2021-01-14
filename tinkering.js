66724caf7ff54577bac13aabef6f84543fdc2ce9
Yamac Ozdil <yamac.ozdil@outlook.com>
2021-01-14 21:35:43 +0000

    Nested for loop

--- tinkering.js
+++ tinkering.js
@@ -1,7 +1,5 @@
-var solution = 1
-
-for (var i = 1; i <= 4; i++) {
-  solution *= i;
+for (var row = 0; row <= 25; row++) {
+  for (var column = 0; column <= 99; column++) {
+  console.log(row + "-" + column); 
+  }
 }
-
-console.log (solution);
