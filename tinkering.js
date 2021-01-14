7ebfe1a034eb012067d1f4896754106d2f253c0b
Yamac Ozdil <yamac.ozdil@outlook.com>
2021-01-14 21:28:58 +0000

    For loop to calculate Factorials

--- tinkering.js
+++ tinkering.js
@@ -1,21 +1,7 @@
-var count = 60;
+var solution = 1
 
-while (count >= 0) {
-    if (count === 50) {
-    console.log ("Orbiter transfers from ground to internal power")
-  } else if (count === 31) {
-    console.log ("Ground launch sequencer is go for auto sequence start")
-  } else if (count === 16) {
-    console.log ("Activate launch pad sound suppression system")
-  } else if (count === 10) {
-    console.log ("Activate main engine hydrogen burnoff system")
-  } else if (count === 6) {
-    console.log ("Main engine start")
-  } else if (count === 0) {
-    console.log ("Solid rocket booster ignition and liftoff!")
-  } else {
-    console.log ("T-" + count + " seconds")
-  }
-
-  count = count - 1;
+for (var i = 1; i <= 4; i++) {
+  solution *= i;
 }
+
+console.log (solution);
