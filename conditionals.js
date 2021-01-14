52c46c2d98a6e230a159d769090bdb817bdabb39
Yamac Ozdil <yamac.ozdil@outlook.com>
2021-01-14 18:49:48 +0000

    Commented out the code

--- conditionals.js
+++ conditionals.js
@@ -1,47 +1,47 @@
 const raining = true;
 const cold = false;
 
-if (raining) {
-  console.log("Don't forget your umbrella!");
-}
+// if (raining) {
+//   console.log("Don't forget your umbrella!");
+// }
 
-if (cold) {
-  console.log("Make sure you pick out a scarf!");
-}
+// if (cold) {
+//   console.log("Make sure you pick out a scarf!");
+// }
 
-console.log("Now you're ready to go outside!");
-
-
-if (cold) {
-  console.log("Make sure you pick out a scarf!");
-} else {
-  console.log("Short sleeves are fine.");
-}
+// console.log("Now you're ready to go outside!");
 
 
-const temperature = 12;
-
-if (temperature < 0) {
-  console.log("Make sure you pick out a scarf!");
-} else if (temperature < 15) {
-  console.log("Short sleeves won't cut it!");
-} else {
-  console.log("Short sleeves are fine.");
-}
-
-console.log("Now you're ready to go outside!");
-
-const isCitizen = true;
-const age = 26;
-
-if (isCitizen && age > 18) {
-  console.log("You are eligible to vote.");
-}
+// if (cold) {
+//   console.log("Make sure you pick out a scarf!");
+// } else {
+//   console.log("Short sleeves are fine.");
+// }
 
 
-if (temperature < -40 || temperature > 40) {
-  console.log("Maybe going outside isn't such a great idea…");
-}
+// const temperature = 12;
+
+// if (temperature < 0) {
+//   console.log("Make sure you pick out a scarf!");
+// } else if (temperature < 15) {
+//   console.log("Short sleeves won't cut it!");
+// } else {
+//   console.log("Short sleeves are fine.");
+// }
+
+// console.log("Now you're ready to go outside!");
+
+// const isCitizen = true;
+// const age = 26;
+
+// if (isCitizen && age > 18) {
+//   console.log("You are eligible to vote.");
+// }
+
+
+// if (temperature < -40 || temperature > 40) {
+//   console.log("Maybe going outside isn't such a great idea…");
+// }
 
 
 
