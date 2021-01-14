083ba1291fa7fdfa5daadc2ae3dc4a89331b1f8c
Yamac Ozdil <yamac.ozdil@outlook.com>
2021-01-14 20:45:00 +0000

    Bottles of Juice Song Be careful about the difference between = and === when it comes to conditions.

--- tinkering.js
+++ tinkering.js
@@ -1,13 +1,12 @@
-var x = 1;
-while (x <= 20) {
-  if (x % 3 === 0 && x % 5 === 0) {
-    console.log("JuliaJames"); }
-  else if (x % 5 === 0) {
-    console.log("James"); }
-  else if (x % 3 === 0) {
-    console.log("Julia"); }
-  else {
-    console.log(x); }
-    
-  x = x + 1;
+var num = 99;
+
+while (num > 0) {
+    if (num = 2) {
+      console.log( num + " bottles of juice on the wall! " + num + " bottles of juice! Take one down, pass it around... " + (num-1) + " bottle of juice on the wall!") }
+    else if (num = 1) {
+      console.log( num + " bottle of juice on the wall! " + num + " bottle of juice! Take one down, pass it around... " + (num-1) + " bottles of juice on the wall!") }
+    else {
+      console.log( num + " bottles of juice on the wall! " + num + " bottles of juice! Take one down, pass it around... " + (num-1) + " bottles of juice on the wall!")
+    }
+    num = num - 1;
 }
