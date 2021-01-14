b2863ce60c22f004751a1e489b69531bc973955b
Yamac Ozdil <yamac.ozdil@outlook.com>
2021-01-14 20:07:16 +0000

    JuliaJames loop

--- tinkering.js
+++ tinkering.js
@@ -1,6 +1,13 @@
-let x = 1;
-
-while (x <= 10000) {
-  console.log(x + " Missisippi!")
+var x = 1;
+while (x <= 20) {
+  if (x % 3 === 0 && x % 5 === 0) {
+    console.log("JuliaJames"); }
+  else if (x % 5 === 0) {
+    console.log("James"); }
+  else if (x % 3 === 0) {
+    console.log("Julia"); }
+  else {
+    console.log(x); }
+    
   x = x + 1;
 }
