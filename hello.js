213bcae3449b6560c2581a28abda2619e46f6559
Yamac Ozdil <yamac.ozdil@outlook.com>
2021-01-15 16:58:55 +0000

    Say Hello Function

---
+++ hello.js
@@ -0,0 +1,17 @@
+// // Hello World function
+
+// const sayHello  = function () {
+//   console.log("Hello, world");
+// }
+
+// sayHello();
+
+// Hello + name
+
+const sayHello  = function (name) {
+  console.log("Hello, " + name);
+}
+
+sayHello("Caliban");
+sayHello("Miranda");
+sayHello("Ferdinand");
