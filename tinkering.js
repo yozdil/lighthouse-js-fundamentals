9d5589b53221c66d7d346c7b94059987ae57c7e8
Yamac Ozdil <yamac.ozdil@outlook.com>
2021-01-15 20:09:13 +0000

    X MArks the perfect shot exercise

--- tinkering.js
+++ tinkering.js
@@ -1,23 +1,19 @@
-const stations = [
-  ['Big Bear Donair', 10, 'restaurant'],
-  ['Bright Lights Elementary', 50, 'school'],
-  ['Moose Mountain Community Centre', 45, 'community centre']
-];
+const moves = ['north', 'north', 'west', 'west', 'north', 'east','north']; 
 
-const chooseStations = function (stations) {
-  const goodStations = [];
-  for (const station of stations) {
-    const capacity = station[1];
-
-    if (capacity >= 20) {
-      const type = station[2];
-      
-      if (type === "school" || type === "community centre") {
-        goodStations.push(station[0]);
-      }
-    }
+const finalPosition = function (moves) {
+  let position = [0,0];
+  for (const move of moves) {
+    if (move === "north") {
+      position[1]++; }
+    else if (move === "south") {
+      position[1]--; }
+    else if (move === "west") {
+      position[0]--; }
+    else {
+      position[0]++; }
   }
-  return goodStations
-}  
-
-chooseStations(stations);
+  return position;
+}
+  
+  
+finalPosition(moves);
