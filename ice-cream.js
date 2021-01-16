01716be8ca6f6107e5076a076d58312cd495b000
Yamac Ozdil <yamac.ozdil@outlook.com>
2021-01-16 14:12:50 +0000

    Packing list Exercise

--- ice-cream.js
+++ ice-cream.js
@@ -2,7 +2,7 @@
 const iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
 console.log(iceCreamFlavours);
 
-Your code below here...
+// Your code below here...
 
 iceCreamFlavours.push("root beer");
 console.log(iceCreamFlavours);
--- tinkering.js
+++ tinkering.js
@@ -1,12 +1,1 @@
-const director = "Steven Spielberg";
-const films = ["Jaws", "Raiders of the Lost Ark", "A.I."];
-
-films[2] = "Jurassic Park";
-films;
-films.push("A.I.");
-console.log(films);
-
-
-films.push("E.T.");
-
-console.log(director + " is the director of " + films.length + " films");
+console.log("Bob" || " Marley")
